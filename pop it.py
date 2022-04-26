db1cf29d9e2919d9153d520ebe82061322250e71
Cai winson <84001376+Caiwinson@users.noreply.github.com>
2022-04-26 19:11:53 +0700

    update pop it.py

--- pop it.py
+++ pop it.py
@@ -1,30 +1,31 @@
 import discord
+print(discord.__file__)
+from discord.ext import commands
 from discord_components import DiscordComponents, Button, ButtonStyle
-client=discord.Client()
-DiscordComponents(client)
+client=commands.Bot(command_prefix="-",intents=discord.Intents.all())
 def get_token():
     with open('token.txt', 'r') as f:
         return f.read()
 @client.event
 async def on_ready():
+    DiscordComponents(client)
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 @client.event
 async def on_button_click(res):
-    if res.component.label=="Play":
+    if res.component.label == 'Play':
         count=0
         b=[]
-        for i in range(6):
+        for i in range(5):
             a=[]
-            for j in range(6):
+            for j in range(5):
                 count+=1
-                a.append(Button(label=str(count), style=ButtonStyle.green))
+                a.append(Button(label=str(count), style=ButtonStyle.green, custom_id=str(res.author.id)))
             b.append(a)
-    await res.respond("test", components=b)
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        await message.channel.send("test", components=[Button(label="Play", style=ButtonStyle.green)])
+        await res.respond(type=4,content="test", components=b)
+@client.command()
+async def test(ctx):
+    await ctx.send("test", components=[Button(label="Play", style=ButtonStyle.green)])
 client.run(get_token())
