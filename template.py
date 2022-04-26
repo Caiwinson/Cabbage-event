20f911b8b0c853822a648de1fcc0db94706640fe
Cai winson <84001376+Caiwinson@users.noreply.github.com>
2022-04-26 17:46:47 +0700

    first commit

---
+++ template.py
@@ -0,0 +1,19 @@
+import discord
+from discord_components import DiscordComponents, Button, ButtonStyle
+client=discord.Client()
+DiscordComponents(client)
+def get_token():
+    with open('token.txt', 'r') as f:
+        return f.read()
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+@client.event
+async def on_button_click(res):
+    if res.component.label=="Play":
+        #code for play and what
+        pass
+client.run(get_token())
