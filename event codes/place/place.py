634a0dc4dd0ea243f3c23bdee4331d422f661ab7
Cai winson <84001376+Caiwinson@users.noreply.github.com>
2022-04-28 11:36:04 +0700

    updates

--- event codes/place/place.py
+++ event codes/place/place.py
@@ -23,25 +23,24 @@ async def on_ready():
     print(client.user.id)
     print('------')
 @client.event
-async def on_button_click(ctx):
-    #a=b[(b.index(a)+1)%len(b)]
-    image=Image.open("C:/Users/User/Desktop/events/event codes/place/full.png")
-    #l=["red", "green", "blue", "yellow", "purple", "orange", "black", "white"]
-    l=["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#ff8000", "#000000", "#ffffff"]
-    b=[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]
-    for i in range(5):
-        for j in range(5):
-            print(b[i][j])
-            if b[i][j]==int(ctx.component.label):
-                image.paste(Image.new('RGB', (200,200), getcolor(db[ctx.component.label], "RGB")), (j*200,i*200))
-                image.save("C:/Users/User/Desktop/events/event codes/place/full.png")
-                db[ctx.component.label]=l[(l.index(db[ctx.component.label])+1)%len(l)]
-                print(db[ctx.component.label])
-                break
-    await ctx.respond(type=7,content="http://192.168.100.15:8080/full.png")
+async def on_button_click(res):
+        #a=b[(b.index(a)+1)%len(b)]
+        image=Image.open("C:/Users/User/Desktop/events/event codes/place/full.png")
+        #l=["red", "green", "blue", "yellow", "purple", "orange", "black", "white"]
+        l=["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#ff8000", "#000000", "#ffffff"]
+        b=[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]
+        for i in range(5):
+            for j in range(5):
+                print(b[i][j])
+                if b[i][j]==int(res.component.label):
+                    image.paste(Image.new('RGB', (200,200), getcolor(db[res.component.label], "RGB")), (j*200,i*200))
+                    image.save("C:/Users/User/Desktop/events/event codes/place/full.png")
+                    db[res.component.label]=l[(l.index(db[res.component.label])+1)%len(l)]
+                    print(db[res.component.label])
+                    break
+        await res.respond(type=7,files=discord.file("C:/Users/User/Desktop/events/event codes/place/full.png"), suppress=True)
 @client.command()
 async def test(ctx):
-    id="".join(random.sample("qwertyuiopasdfghjklzxcvbnm", 8))
     count=0
     b=[]
     for i in range(5):
