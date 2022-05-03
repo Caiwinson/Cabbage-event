27382df847b1f4ec36f22dba2c013f8483144464
Cai winson <84001376+Caiwinson@users.noreply.github.com>
2022-05-03 17:10:58 +0700

    Delete unnecesarry cache

--- event codes/place/Untitled-2.py
+++
@@ -1,4 +0,0 @@
-c={"red": "#E50000", "green": "#02BE01", "blue": "#0000EA", "yellow": "#E5D900", "magenta": "#820080", "orange": "#E59500", "black": "#222222", "white": "#ffffff", "lime": "#94E044", "cyan": "#00D3DD", "cyan-blue": "#0083C7", "lavender": "#CF6EE4", "gray": "#888888", "light-gray": "#E4E4E4", "pink": "#FFA7D1", "brown": "#A06A42"}
-#reverse c
-d={v:k for k,v in c.items()}
-print(d)
