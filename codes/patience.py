eb606ecc25c7054830de33bb90a9d000d81a6a12
Cai winson <84001376+Caiwinson@users.noreply.github.com>
2022-07-21 20:18:43 +0700

    Create patience.py

---
+++ codes/patience.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+from discord_components import DiscordComponents, Button, ButtonStyle
+from random import randint
+import asyncio
+import time
+client=commands.Bot(command_prefix='!', intents=discord.Intents.all())
+def get_token():
+    with open('token.txt', 'r') as f:
+        return f.read()
+@client.event
+async def on_ready():
+    DiscordComponents(client)
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+@client.event
+async def on_button_click(res):
+    print(res)
+    if res.component.label in ["Play", "Play Again"]:
+        if res.component.label == "Play":
+            try:
+                msg=await res.author.send("Are you ready", components=[Button(label="yes", style=ButtonStyle.green)])
+            except discord.errors.Forbidden:
+                await res.respond(type=4, content="Please enable DM's in your setting")
+                return
+            def check(m):
+                return m.message==msg
+            await res.respond(type=6)
+            res=await client.wait_for('button_click', check=check)
+        else:
+            msg=res.message
+            def check(m):
+                return m.message==msg
+        embed=discord.Embed(description="Do not click the till the button turn green\n<a:loading:994538965547884676>")
+        await res.respond(type=7,content="",embed=embed,components=[Button(label="Click me")])
+        try:
+            ctx=await client.wait_for("button_click",check=check, timeout=randint(5,15))
+        except asyncio.TimeoutError:
+            pass
+        else:
+            embed.colour=0xff0000
+            await ctx.respond(type=7,content="You clicked the button when it's still grey", embed=embed, components=[Button(label="Play Again", style=ButtonStyle.blue)])
+            return
+        await msg.edit(embed=discord.Embed(description="Do not click the till the button turn green\n✅"),components=[Button(label="Click me",style=ButtonStyle.green)])
+        t=time.time()
+        ctx=await client.wait_for('button_click', check=check)
+        embed.colour=0x00ff00
+        embed.set_footer(text=f"latency: {round(time.time-t,2)} seconds")
+        await ctx.respond(type=7,embed=embed, components=[Button(label="Play Again", style=ButtonStyle.blue)])
+@client.command()
+async def test(ctx):
+    await ctx.send("test", components=[Button(label="Play", style=ButtonStyle.green)])
+client.run(get_token())
