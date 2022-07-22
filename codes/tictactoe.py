7ee1d530a2c8ce7ce2672ac5b3df135930dc6a01
Cai winson <84001376+Caiwinson@users.noreply.github.com>
2022-07-22 21:59:22 +0700

    completed tic tac toe

--- codes/tictactoe.py
+++ codes/tictactoe.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord_components import Button, ButtonStyle
 import asyncio
+import random
 class TTT(commands.Cog, name="TTT"):
     def __init__(self, client):
         self.client=client
@@ -9,26 +10,116 @@ class TTT(commands.Cog, name="TTT"):
     async def on_button_click(self,res):
         if res.component.label in ["Play", "Play Again"]:
             async def tictactoe(msg, player1,player2):
-                player1=player1.id
-                player2=player2.id
+                def is_player_win(player):
+                    win = None
+
+                    n = len(board)
+
+                    # checking rows
+                    for i in range(n):
+                        win = True
+                        for j in range(n):
+                            if board[i][j] != player:
+                                win = False
+                                break
+                        if win:
+                            return win
+
+                    # checking columns
+                    for i in range(n):
+                        win = True
+                        for j in range(n):
+                            if board[j][i] != player:
+                                win = False
+                                break
+                        if win:
+                            return win
+
+                    # checking diagonals
+                    win = True
+                    for i in range(n):
+                        if board[i][i] != player:
+                            win = False
+                            break
+                    if win:
+                        return win
+
+                    win = True
+                    for i in range(n):
+                        if board[i][n - 1 - i] != player:
+                            win = False
+                            break
+                    if win:
+                        return win
+                    return False
+
+                def is_board_filled():
+                    for row in board:
+                        for item in row:
+                            if item == '-':
+                                return False
+                    return True
+                async def board_msg():
+                    b=""
+                    buttons=[]
+                    for i,y in zip(board,range(3)):
+                        e=""
+                        c=[]
+                        for a,x in zip(i, range(3)):
+                            e+=f":{a}:"
+                            c.append(Button(label=a.replace("-", "⠀").upper(), style=ButtonStyle.green, disabled=a!="-", custom_id=f"{x} {y}"))
+                        b+=e+"\n"
+                        buttons.append(c)
+                    if status.endswith("Win") or status.endswith("Tied"):
+                        buttons=[]
+                    embed=discord.Embed(description=f"{status}\n\n⭕={player1}\n❌={player2}")
+                    embed.add_field(name="⠀", value=b.replace(":-:", "⬛"))
+                    await msg.edit(embed=embed)
+                    await player1_msg.edit(embed=embed,components=buttons, content="")
+                    await player2_msg.edit(embed=embed,components=buttons, content="")
+                player1=player1
+                player2=player2
                 board=[]
                 for _ in range(3):
                     c=[]
                     for _ in range(3):
                         c.append("-")
                     board.append(c)
-                async def board_msg():
-                    status="HA"
-                    b=""
-                    for i in board:
-                        e=""
-                        for a in i:
-                            e+=f":{a}:"
-                        b+=e+"\n"
-                    embed=discord.Embed(description=f"{status}")
-                    embed.add_field(name="⠀", value=b.replace(":-:", "<:blank:872313293342138418>"))
-                    await msg.edit(embed=embed)
+                player1_msg=m
+                player2_msg=await player2.send("Please wait")
+                await msg.edit(components=[])
+                players={
+                    "o": {"user": player1,"msg": player1_msg},
+                    "x": {"user": player2,"msg": player2_msg}
+                }
+                cr=random.choice(["o", "x"])
+                status=f"{players[cr]['user']} turns"
                 await board_msg()
+                while True:
+                    ctx=await self.client.wait_for("button_click")
+                    if ctx.message==players[cr]['msg']:
+                        pass
+                    elif ctx.message==players['x' if cr == 'o' else 'o']['msg']:
+                        await ctx.respond(type=4, content="It's not your turn yet")
+                        continue
+                    else:
+                        continue
+                    await ctx.respond(type=6)
+                    xy=ctx.component.id.split()
+                    board[int(xy[1])][int(xy[0])]=cr
+                    if is_player_win(cr):
+                        status=f"{players[cr]['user']} Win"
+                        await board_msg()
+                        break
+
+                    # checking whether the game is draw or not
+                    if is_board_filled():
+                        status="Game Tied"
+                        await board_msg()
+                        break
+                    cr='x' if cr == 'o' else 'o'
+                    status=f"{players[cr]['user']} turns"
+                    await board_msg()
             await res.respond(type=4,content="Check your DM")
             m=await res.author.send("Would you like to host or join a match", components=[[Button(label="Host", style=ButtonStyle.green), Button(label="Join", style=ButtonStyle.blue)]])
             a=await self.client.wait_for("button_click", check=lambda i:i.message==m)
@@ -53,6 +144,7 @@ class TTT(commands.Cog, name="TTT"):
                         await ctx.respond(type=4, content="Check your DM")
                         await msg.edit(content=f"Spectating {res.author.mention} vs {ctx.author.mention}")
                         await tictactoe(msg, res.author, ctx.author)
+                        break
 
 
 def setup(client):
